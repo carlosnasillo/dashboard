bc52caed79bea0d0da3c6751229edbbfd98cf7ef
Julien DERAY <julien@deray.fr>
2015-10-23 17:34:15 +0100

    implements trivial set up of AngularJS

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
+import java.io.File
+
 import play.api._
 import play.api.mvc._
+import play.api.Play.current
 
 class Application extends Controller {
 
   def index = Action {
     Ok("")
+  }
+
+  /** resolve "any" into the corresponding HTML page URI */
+  def getURI(any: String): String = any match {
+    case "mainDashboard" => "/public/html/mainDashboard.html"
+    case _ => "error"
+  }
+
+  /** load an HTML page from public/html */
+  def loadPublicHTML(any: String) = Action {
+    val projectRoot = Play.application.path
+    val file = new File(projectRoot + getURI(any))
+    if (file.exists())
+      Ok(scala.io.Source.fromFile(file.getCanonicalPath).mkString).as("text/html")
+    else
+      NotFound
   }
 
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,33 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+var app = angular.module("app", ["ngResource", "ngRoute"])
+    .constant("apiUrl", "/api")
+    .config(["$routeProvider", function($routeProvider) {
+      return $routeProvider.when("/", {
+        templateUrl: "/views/mainDashboard",
+        controller: "MainDashboardCtrl"
+      }).otherwise({
+        redirectTo: "/"
+      });
+    }
+    ]).config([
+      "$locationProvider", function($locationProvider) {
+        return $locationProvider.html5Mode({
+          enabled: true,
+          requireBase: false
+        }).hashPrefix("!"); // enable the new HTML5 routing and history API
+        // return $locationProvider.html5Mode(true).hashPrefix("!"); // enable the new HTML5 routing and history API
+      }
+    ]);
+
+// the global controller
+app.controller("AppCtrl", ["$scope", "$location", function($scope, $location) {
+  // the go function is inherited by all other controllers
+  $scope.go = function (path) {
+    $location.path(path);
+  };
+}]);
+
+// the list controller
+app.controller("MainDashboardCtrl", ["$scope", "$resource", "apiUrl", function($scope, $resource, apiUrl) {
+  var Analytics = $resource(apiUrl + "/analytics"); // a RESTful-capable resource object
+  $scope.analytics = Analytics.query(); // for the list of analytics in public/html/mainDashboard.html
+}]);
