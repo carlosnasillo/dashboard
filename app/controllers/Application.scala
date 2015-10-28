df4f471711358bded88e8253e8926410f459b1d1
Julien DERAY <julien@deray.fr>
2015-10-28 11:35:29 +0000

    adds copyright note

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2015 Lattice Markets, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * Lattice Markets, except with written permission of Lattice Markets.
+ *
+ */
+
 package controllers
 
 import java.io.File
