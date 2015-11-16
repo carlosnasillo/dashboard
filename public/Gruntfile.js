ffed85fd93eee24dbf1b5a80848374542ef6345d
Julien DERAY <julien@deray.fr>
2015-11-16 13:13:26 +0000

    Sets up Grunt to automatically concat & minify js files

---
+++ public/Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+        options: {
+            separator: ';'
+        },
+        dist: {
+            src: ['app/app.js', 'app/**/*.js'],
+            dest: 'dist/<%= pkg.name %>.js'
+        }
+    },
+    uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
+        dist: {
+            files: {
+                'dist/<%= pkg.name %>.js': ['<%= concat.dist.dest %>']
+            },
+            options: {
+                mangle: false,
+                compress: true
+            }
+        },
+        bower: {
+            files: {
+                'dist/bower.js': 'dist/bower.js'
+            },
+            options: {
+                mangle: false,
+                compress: true
+            }
+        }
+    },
+    bower_concat: {
+        all: {
+            dest: 'dist/bower.js',
+            mainFiles: {
+                'font-awesome': 'css/font-awesome.min.css',
+                'Flot': ['jquery.flot.js', 'jquery.flot.resize.js', 'jquery.flot.pie.js']
+            }
+        }
+    },
+    bower: {
+        install: {
+            options: {
+                install: true,
+                copy: false,
+                targetDir: './libs',
+                cleanTargetDir: true
+            }
+        }
+    }
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-bower-concat');
+
+    grunt.registerTask('default', ['bower', 'concat', 'uglify:dist', 'bower_concat', 'uglify:bower']);
+    grunt.registerTask('dev', ['bower', 'concat', 'bower_concat']);
+};
