2bbab8d9a6ea6d065a47e6ce79a04b91ed65fb4a
Julien DERAY <julien@deray.fr>
2015-12-11 11:53:25 +0000

    Sets up quotes storage API

---
+++ app/models/Quote.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015 Lattice Markets, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * Lattice Markets, except with written permission of Lattice Markets.
+ *
+ */
+
+package models
+
+import com.lattice.lib.utils.DbUtil
+import org.joda.time.DateTime
+import play.api.Logger
+import play.api.libs.json.{JsObject, Json}
+import play.modules.reactivemongo.json._
+import play.modules.reactivemongo.json.collection.JSONCollection
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
+/**
+  * @author : julienderay
+  * Created on 11/12/2015
+  */
+
+case class Quote(
+                  id: String,
+                  rfqId: String,
+                  timestamp: DateTime,
+                  premium: BigDecimal,
+                  timeWindowInMinutes: Int,
+                  client: String,
+                  dealer: String
+                )
+
+object Quote {
+  val collectionName = "quotes"
+
+  implicit val quoteFormat = Json.format[Quote]
+
+  val quotesTable: JSONCollection = DbUtil.db.collection(collectionName)
+
+  def store(quote: Quote) {
+    val future = quotesTable.insert(Json.toJson(quote).as[JsObject])
+    future.onComplete {
+      case Failure(e) => throw e
+      case Success(lastError) => Logger.info(s"New quote inserted : $quote")
+    }
+  }
+}
