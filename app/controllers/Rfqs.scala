16bcf8a0a01b3f9f69c3c05aab9c3b2da14dc470
Julien DERAY <julien@deray.fr>
2015-12-11 11:21:11 +0000

    Sets up RFQs storage API

---
+++ app/controllers/Rfqs.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 Lattice Markets, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * Lattice Markets, except with written permission of Lattice Markets.
+ *
+ */
+
+package controllers
+
+import models.Rfq
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc._
+
+/**
+  * @author : julienderay
+  * Created on 11/12/2015
+  */
+
+class Rfqs extends Controller {
+
+  val rfqForm = Form(
+    mapping (
+      "id" -> nonEmptyText,
+      "timestamp" -> jodaDate("yyyy-MM-dd HH:mm:ss"),
+      "durationInMonths" -> number,
+      "client" -> nonEmptyText,
+      "dealer" -> list(nonEmptyText),
+      "creditEvent" -> list(nonEmptyText),
+      "timeWindowInMinutes" -> number,
+      "isValid" -> boolean,
+      "cdsValue" -> bigDecimal
+    )(Rfq.apply)(models.Rfq.unapply)
+  )
+
+  def submitRFQ = Action { implicit request =>
+    rfqForm.bindFromRequest.fold(
+      formWithErrors => {
+        BadRequest("Wrong data sent.")
+      },
+      submittedRfq => {
+        Rfq.store(submittedRfq)
+        Ok
+      }
+    )
+  }
+}
