b1c1273022f08583ace5f0a9f54b189289e2c2db
Julien DERAY <julien@deray.fr>
2015-12-22 13:43:41 +0000

    Adds 'enter' control to submit forms in quote and RFQ modals

---
+++ public/app/directives/ngEnter/ngEnter.directive.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2015 Lattice Markets, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * Lattice Markets, except with written permission of Lattice Markets.
+ *
+ */
+
+/**
+* @author : julienderay
+* Created on 22/12/2015
+*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('ngEnter', function () {
+            return function (scope, elements, attrs) {
+                elements.bind('keydown keypress', function (event) {
+                    if (event.which === 13) {
+                        scope.$apply(function () {
+                            scope.$eval(attrs.ngEnter);
+                        });
+                        event.preventDefault();
+                    }
+                });
+            };
+        });
+})();
