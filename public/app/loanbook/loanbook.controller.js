a7881eeddc7dcb269521304239326608b49de1bb
Julien DERAY <julien@deray.fr>
2015-12-29 13:15:06 +0000

    Implements filters on loanbook

--- public/app/loanbook/loanbook.controller.js
+++ public/app/loanbook/loanbook.controller.js
@@ -19,17 +19,24 @@
         .module('app')
         .controller('LoanBookController', LoanBookController);
 
-    LoanBookController.$inject = ['LoanBookService', 'LoanBookTableService', 'RfqModalService', '$routeParams', '$timeout'];
+    LoanBookController.$inject = ['LoanBookService', 'LoanBookTableService', 'RfqModalService', '$routeParams', '$timeout', 'GridTableUtil', '$filter', '$scope'];
 
-    function LoanBookController(LoanBookService, LoanBookTableService, RfqModalService, $routeParams, $timeout) {
+    function LoanBookController(LoanBookService, LoanBookTableService, RfqModalService, $routeParams, $timeout, GridTableUtil, $filter, $scope) {
         var vm = this;
 
         vm.loanBookTable = {};
         vm.loanBookTable.options = LoanBookTableService.options;
-
+        vm.originalData = [];
 
         LoanBookService.loanBookData().then(function(data) {
+            data.map(function(loan) {
+                loan.loanDateToFormattedDate = $filter('date')(loan.loanDate, "dd/MM/yyyy");
+                loan.interestPerCent = loan.interest * 100;
+                return loan;
+            });
+
             vm.loanBookTable.options.data = data;
+            vm.originalData = data;
 
             $timeout(function() {
                 if (vm.loanBookTable.gridApi.selection.selectRow) {
@@ -45,11 +52,56 @@
                 }
             });
         });
+
         vm.loanBookTable.options.onRegisterApi = function(gridApi) {
             vm.loanBookTable.gridApi = gridApi;
         };
 
-
         vm.order = RfqModalService.orderModal;
+
+        vm.loanBookTable.filters = {};
+        vm.loanBookTable.filters.loanDate = GridTableUtil.dateFilterFactory(vm.originalData, function(filteredData) { vm.loanBookTable.options.data = filteredData; }, 'loanDateToFormattedDate');
+
+        vm.loanBookTable.filters.filterLoans = function () {
+            vm.loanBookTable.options.data = vm.originalData.filter(function (loanObj) {
+                return vm.loanBookTable.filters.originator.filterFn(loanObj) &&
+                vm.loanBookTable.filters.status.filterFn(loanObj) &&
+                vm.loanBookTable.filters.grade.filterFn(loanObj) &&
+                vm.loanBookTable.filters.purpose.filterFn(loanObj) &&
+                vm.loanBookTable.filters.sector.filterFn(loanObj) &&
+                vm.loanBookTable.filters.type.filterFn(loanObj) &&
+                vm.loanBookTable.filters.region.filterFn(loanObj) &&
+                vm.loanBookTable.filters.amount.start.filterFn(loanObj) &&
+                vm.loanBookTable.filters.amount.end.filterFn(loanObj) &&
+                vm.loanBookTable.filters.interestPerCent.start.filterFn(loanObj) &&
+                vm.loanBookTable.filters.interestPerCent.end.filterFn(loanObj) &&
+                vm.loanBookTable.filters.term.start.filterFn(loanObj) &&
+                vm.loanBookTable.filters.term.end.filterFn(loanObj) &&
+                vm.loanBookTable.filters.security.filterFn(loanObj);
+            });
+            GridTableUtil.applyDateFilter(
+                vm.loanBookTable.filters.loanDate.start.value,
+                vm.loanBookTable.filters.loanDate.end.value,
+                'loanDateToFormattedDate',
+                vm.loanBookTable.options.data,
+                function (filteredData) {
+                    vm.loanBookTable.options.data = filteredData;
+                });
+        };
+
+        vm.loanBookTable.filters.originator = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'originator');
+        vm.loanBookTable.filters.status = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'status');
+        vm.loanBookTable.filters.grade = GridTableUtil.wordFilterFactory(vm.loanBookTable.filters.filterLoans, 'grade');
+        vm.loanBookTable.filters.purpose = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'purpose');
+        vm.loanBookTable.filters.sector = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'sector');
+        vm.loanBookTable.filters.type = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'type');
+        vm.loanBookTable.filters.region = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'region');
+        vm.loanBookTable.filters.amount = GridTableUtil.doubleNumberFilterFactory(vm.loanBookTable.filters.filterLoans, 'amount');
+        vm.loanBookTable.filters.interestPerCent = GridTableUtil.doublePercentFilterFactory(vm.loanBookTable.filters.filterLoans, 'interestPerCent');
+        vm.loanBookTable.filters.term = GridTableUtil.doubleNumberFilterFactory(vm.loanBookTable.filters.filterLoans, 'term');
+        vm.loanBookTable.filters.security = GridTableUtil.textFilterFactory(vm.loanBookTable.filters.filterLoans, 'security');
+
+        $scope.$watch('vm.loanBookTable.filters.loanDate.start.value', vm.loanBookTable.filters.filterLoans, false);
+        $scope.$watch('vm.loanBookTable.filters.loanDate.end.value', vm.loanBookTable.filters.filterLoans, false);
     }
 })();
