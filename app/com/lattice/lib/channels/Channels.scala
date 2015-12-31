f081ed36a48f8b0d8306ced0154f21b88589de21
Julien DERAY <julien@deray.fr>
2015-12-31 17:32:14 +0000

    Implements simple automatic quote by Lattice

---
+++ app/com/lattice/lib/channels/Channels.scala
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015 Lattice Markets, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * Lattice Markets, except with written permission of Lattice Markets.
+ *
+ */
+
+package com.lattice.lib.channels
+
+import play.api.libs.iteratee.{Iteratee, Concurrent}
+import play.api.libs.json.JsValue
+
+/**
+  * @author : julienderay
+  * Created on 31/12/2015
+  */
+
+object Channels {
+
+  val ignoredIn = Iteratee.ignore[JsValue]
+  val (outRfq, channelRfq) = Concurrent.broadcast[JsValue]
+  val (outQuotes, channelQuotes) = Concurrent.broadcast[JsValue]
+}
