f6a810fee6b38ce2522c4e9ac72e8d7c207423ee
ze97286 <zohar.etzioni@gmail.com>
2016-01-07 11:44:58 +0000

    MVP-99 changed lattice markets to PBX Technology + Lattice (autoquoter) to PDX

--- app/AppGlobal.scala
+++ app/AppGlobal.scala
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015 PDX Technology, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * PDX Technology, except with written permission of PDX Technology.
+ *
+ */
 import com.lattice.lib.integration.lc.LendingClubFactory
 import play.api.{GlobalSettings, Logger}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
--- app/com/lattice/lib/utils/Implicits.scala
+++ app/com/lattice/lib/utils/Implicits.scala
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2015 PDX Technology, All rights reserved.
+ *
+ * Unpublished copyright. All rights reserved. This material contains
+ * proprietary information that shall be used or copied only with
+ * PDX Technology, except with written permission of PDX Technology.
+ */
 package com.lattice.lib.utils
 
 /**
