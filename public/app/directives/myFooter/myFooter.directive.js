37fd3d1754caa2b5f82a086a69518dc9c0f775ec
ze97286 <zohar.etzioni@gmail.com>
2016-01-07 12:23:30 +0000

    MVP-99 more lattice->PDX

--- public/app/directives/myFooter/myFooter.directive.js
+++ public/app/directives/myFooter/myFooter.directive.js
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2015 Lattice Markets, All rights reserved.
+ * Copyright (c) 2015 PDX Technology, All rights reserved.
  *
  * Unpublished copyright. All rights reserved. This material contains
  * proprietary information that shall be used or copied only with
- * Lattice Markets, except with written permission of Lattice Markets.
+ * PDX Technology, except with written permission of PDX Technology.
  *
  */
 
@@ -26,7 +26,7 @@
             restrict: 'E',
             scope: {
             },
-            template: '<div class="footer"><div><strong>Copyright</strong> Lattice Markets &copy; 2014-2015</div></div>'
+            template: '<div class="footer"><div><strong>Copyright</strong> PDX Technology &copy; 2014-2015</div></div>'
         };
     }
 })();
