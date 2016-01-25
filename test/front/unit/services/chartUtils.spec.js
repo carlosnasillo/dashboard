d418be3df0ff5c307ee52b62e0ec180843a86260
Julien Déray <julien@deray.fr>
2016-01-25 13:09:57 +0000

    Tests chartUtils.moveGradeFromValueToKey

--- test/front/unit/services/chartUtils.spec.js
+++ test/front/unit/services/chartUtils.spec.js
@@ -60,4 +60,58 @@ describe('chartUtilsService', function() {
             expect(chartUtils.splitObjectInArray(obj)).toEqual(expectedObject);
         });
     });
+
+    describe('doubleDoubleToPercents', function() {
+        it('should change ";" to "-" and add a % sign', function() {
+            var objectIn = { '3.34;25.54': jasmine.any(Object) };
+            var expectedReturn = { '3.34-25.54%': jasmine.any(Object) };
+
+            expect(chartUtils.doubleDoubleToPercents(objectIn)).toEqual(expectedReturn);
+        });
+
+        it('should not crash if the key is not formed this way', function() {
+            var objectIn = { a: jasmine.any(Object) };
+            var expectedReturn = { a: jasmine.any(Object) };
+
+            expect(chartUtils.doubleDoubleToPercents(objectIn)).toEqual(expectedReturn);
+        });
+    });
+
+    describe('moveGradeFromValueToKey', function() {
+        it('should return the expected result', function() {
+            var inputObj = {
+                AZ: { A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7 },
+                ZD: { A: 8, B: 9, C: 10, D: 11, E: 12, F: 13, G: 14 }
+            };
+            var expectedResult = {
+                A: { AZ: 1, ZD: 8 },
+                B: { AZ: 2, ZD: 9 },
+                C: { AZ: 3, ZD: 10 },
+                D: { AZ: 4, ZD: 11 },
+                E: { AZ: 5, ZD: 12 },
+                F: { AZ: 6, ZD: 13 },
+                G: { AZ: 7, ZD: 14 }
+            };
+
+            expect(chartUtils.moveGradeFromValueToKey(inputObj)).toEqual(expectedResult);
+        });
+
+        it('should work even if all grades are not present', function() {
+            var inputObj = {
+                AZ: { A: 1, B: 2, F: 6, G: 7 },
+                ZD: { A: 8, B: 9, C: 10, D: 11, E: 12, F: 13, G: 14 }
+            };
+            var expectedResult = {
+                A: { AZ: 1, ZD: 8 },
+                B: { AZ: 2, ZD: 9 },
+                C: { ZD: 10 },
+                D: { ZD: 11 },
+                E: { ZD: 12 },
+                F: { AZ: 6, ZD: 13 },
+                G: { AZ: 7, ZD: 14 }
+            };
+
+            expect(chartUtils.moveGradeFromValueToKey(inputObj)).toEqual(expectedResult);
+        });
+    });
 });
